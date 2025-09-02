6c621a17ddfc9acdc8bc081df6b7fa1e470b282b
SaiRam-014112005 <sairamkumardhamodharan@gmail.com>
2025-09-02 15:52:50 +0530

    Second commit remaining

---
+++ models/Users.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.models.User || mongoose.model("User", UserSchema);
---
+++ passport.js
@@ -0,0 +1,74 @@
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const bcrypt = require("bcryptjs");
+const User = require("./models/Users"); 
+
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email });
+      if (!user) return done(null, false, { message: "Incorrect email" });
+
+      if (!user.password) return done(null, false, { message: "Use Google login" });
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) return done(null, false, { message: "Incorrect password" });
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3019/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let existingUser = await User.findOne({ googleId: profile.id });
+        if (existingUser) return done(null, existingUser);
+
+        const emailUser = await User.findOne({ email: profile.emails[0].value });
+        if (emailUser) {
+          emailUser.googleId = profile.id;
+          await emailUser.save();
+          return done(null, emailUser);
+        }
+
+        const newUser = new User({
+          googleId: profile.id,
+          name: profile.displayName,
+          email: profile.emails[0].value,
+        });
+        await newUser.save();
+        done(null, newUser);
+      } catch (err) {
+        done(err, null);
+      }
+    }
+  )
+);
+
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+module.exports = passport;
